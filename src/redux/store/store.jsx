3d4dcfdbf02d4943df73a7a0638be312b521495b
kartikmax <kyadav5213@gmail.com>
2024-06-23 15:28:42 +0530

    removed local storage logic
    Signed-off-by: kartikmax <kyadav5213@gmail.com>

--- src/redux/store/store.jsx
+++ src/redux/store/store.jsx
@@ -1,22 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import wishlistReducer from "../slices/wishList";
 import cartReducer from "../slices/cartSlice";
-import { loadState, saveState } from "../localStorage.js";
-
-const preloadedState = {
-  wishlist: loadState(),
-};
 
 const store = configureStore({
   reducer: {
     wishlist: wishlistReducer,
     cart: cartReducer,
   },
-  preloadedState,
-});
-
-store.subscribe(() => {
-  saveState(store.getState().wishlist);
 });
 
 export default store;
